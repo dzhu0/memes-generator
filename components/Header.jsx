4a2461881d25f9c2994520643f34069af6817922
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-02-11 15:52:44 -0600

    Fix troll face img

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import trollFace from "../images/troll-face.png"
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src={trollFace}
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
